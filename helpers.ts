37e5139e68add2efa97b72106533de3dddf8d780
kmd39 <91749337+kmd39@users.noreply.github.com>
2026-03-05 21:56:53 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:55:36.475Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T18:56:53.538Z
+Commit: Fix CSS styling bug on mobile devices
