971e7a527a49c7d2f1453503847cebf8d93bfd0e
kmd39 <91749337+kmd39@users.noreply.github.com>
2026-03-05 21:55:36 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:53:47.283Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T18:55:36.475Z
+Commit: Introduce real-time notifications
