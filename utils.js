b8415e9f8d7b743eff8b4120b8ea646b5b53bae2
kmd39 <91749337+kmd39@users.noreply.github.com>
2026-03-05 21:56:28 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:56:00.875Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T18:56:27.939Z
+Commit: Resolve null pointer exception in user service
