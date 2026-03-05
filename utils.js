591a5111dbc29038eb1fbdc6e56217729c8e0cf0
kmd39 <91749337+kmd39@users.noreply.github.com>
2026-03-05 21:56:50 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:56:32.051Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T18:56:50.314Z
+Commit: Fix memory leak in event handlers
