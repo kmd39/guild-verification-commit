ddff55387658285729740e2fefee8c81b6946cb4
kmd39 <91749337+kmd39@users.noreply.github.com>
2026-03-05 21:56:55 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:56:50.314Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T18:56:54.786Z
+Commit: Fix broken links in documentation
