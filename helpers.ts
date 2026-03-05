18f6ce5a14815366c874fee965ab3c9c54b4f3e2
kmd39 <91749337+kmd39@users.noreply.github.com>
2026-03-05 21:57:03 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:56:53.538Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T18:57:02.819Z
+Commit: Patch API endpoint error handling
