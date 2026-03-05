d2cf7dc572fa6efc7022168142c9c6a4df25e3d2
kmd39 <91749337+kmd39@users.noreply.github.com>
2026-03-05 21:57:10 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:57:02.819Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T18:57:09.795Z
+Commit: Patch security vulnerability in authentication
